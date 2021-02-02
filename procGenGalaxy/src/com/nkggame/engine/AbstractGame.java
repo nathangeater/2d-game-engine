b4ee78aae409b988ba6252712e16ba2b35edc1ad
Nathan Geater <78389584+nathangeater@users.noreply.github.com>
2021-02-02 11:38:36 -0600

    Delete procGenGalaxy directory

--- procGenGalaxy/src/com/nkggame/engine/AbstractGame.java
+++
@@ -1,6 +0,0 @@
-package com.nkggame.engine;
-
-public abstract class AbstractGame {
-	public abstract void update(GameContainer gc, float dt);
-	public abstract void render(GameContainer gc, Renderer r);
-}
